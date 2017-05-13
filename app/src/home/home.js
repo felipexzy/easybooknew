4050aaee49c0a3a7218c88aeccf5fc78c97ab4ce
felipexzy <felipeeadls@gmail.com>
2017-05-13 20:14:35 -0300

    aaaaaaaaaav3
    aaaaaaaaaaaaaaaaasdasdasdasdv3

--- app/src/home/home.js
+++ app/src/home/home.js
@@ -18,5 +18,19 @@ angular.module('easybook')
             //ngNotify.set('Loaded livros with success.', 'success');
         });
     }
+    
+    $scope.hasAnyPermission = function(authorities) {
+      var hasPermission = false;
+        
+      $rootScope.authDetails.permissions.forEach(function(permission) {
+        authorities.forEach(function(authority) {
+          if (permission.authority === authority) {
+            hasPermission = true;
+          }
+        });
+      });
+
+      return hasPermission;
+    };
 
 });
