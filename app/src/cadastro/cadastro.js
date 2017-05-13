e888a4fe58996e11d21118713fec620d3d6f7d9f
Mizael Santos <mizaelmfs@gmail.com>
2017-05-13 20:34:40 -0300

    cadastro

--- app/src/cadastro/cadastro.js
+++ app/src/cadastro/cadastro.js
@@ -3,4 +3,12 @@
 angular.module('easybook')
 .controller('CadCtrl', function($scope, ngNotify, RestSrv, SERVICE_PATH, $http) {
 
+    var cadastroUrl = SERVICE_PATH.PUBLICO_PATH + '/cadastro';
+
+    $scope.savelivro = function (usuario) {
+    var usuarioUrl = SERVICE_PATH.PUBLICO_PATH + '/cadastro';
+            RestSrv.add(cadastroUrl, usuario, function (newUsuario) {
+                ngNotify.set('Usuario \'' + usuario.email + '\' added.', 'success');
+            });
+        };
 });
