5407a5535626e6ed3abd6b32f53d2b52e1befe58
Mizael Santos <mizaelmfs@gmail.com>
2017-05-13 23:03:37 -0300

    asdweewqe

--- app/src/cadastro/cadastro.js
+++ app/src/cadastro/cadastro.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('easybook')
-.controller('CadCtrl', function($scope, ngNotify, RestSrv, SERVICE_PATH) {
+.controller('CadCtrl', function($scope, ngNotify, RestSrv, SERVICE_PATH, $location) {
 
     var cadastroUrl = SERVICE_PATH.PUBLICO_PATH + '/cadastro';
 
@@ -9,6 +9,11 @@ angular.module('easybook')
     var usuarioUrl = SERVICE_PATH.PUBLICO_PATH + '/cadastro';
             RestSrv.add(cadastroUrl, usuario, function (newUsuario) {
                 ngNotify.set('Usuario \'' + usuario.email + '\' added.', 'success');
+                $location.path('/login');
             });
         };
+
+        $scope.cancelar = function(){
+            $location.path('/');
+        }
 });
