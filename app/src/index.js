6ea82a282d6c955c25df2c9376344752823c8395
felipexzy <felipeeadls@gmail.com>
2017-05-15 23:18:10 -0300

    commmmit
    commmmmit

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var BASE_URL = 'http://localhost:8080';
+var BASE_URL = 'http://easybookalgodaodoce.herokuapp.com';
 
 
 angular.module('easybook', ['checklist-model', 'ngNotify', 'ngRoute', 'ngCookies', 'ngStorage', 
